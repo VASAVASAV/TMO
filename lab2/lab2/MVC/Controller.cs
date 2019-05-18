ad0a4d8c85bf10ebb7ba5985f221b07cec4441f6
VASAVASAV <42357207+VASAVASAV@users.noreply.github.com>
2019-05-18 21:55:33 +0300

    started 2 lab

--- lab2/lab2/MVC/Controller.cs
+++ lab2/lab2/MVC/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,53 @@ namespace lab2
         {
             View = InpForm;
             MyModel = InpModel;
+            MainForm.MyCont = this;
+            Model.MyCont = this;
         }
 
-        public void ReadFile(string NameOfFile, bool NeedsComputing)
+        public void ReadFile(System.Windows.Forms.OpenFileDialog file, bool NeedsComputing)
         {
-
+            //this.ProgressBar.Step = 100;
+            //this.ProgressBar.PerformStep();
+            //this.ProgressBar.Value = 50;
+            List<double> temp = new List<double>();
+            string[] FileLikeStrings = File.ReadAllLines(file.FileName);
+            char[] separators = new char[] { ' ', '\t', '\n' };
+            for (int i = 0; i < FileLikeStrings.Length; i++)
+            {
+                ToolsForWork.RemoveComments(ref FileLikeStrings[i]);
+                string[] valuesInLine = FileLikeStrings[i].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < valuesInLine.Length; j++)
+                {
+                    double x;
+                    try
+                    {
+                        valuesInLine[j] = valuesInLine[j].Replace(".", ",");
+                        x = Convert.ToDouble(valuesInLine[j]);
+                    }
+                    catch
+                    {
+                        View.ShowMessage("Theres mistake in string #" + i);
+                        continue;
+                    }
+                    temp.Add(x);
+                }
+            }
+            temp.Sort();
+            if (NeedsComputing == false)
+            {
+                MyModel.SetData(temp);
+            }
+            else
+            {
+                List<double> intervals = new List<double>();
+                for (int i = 0; i < temp.Count - 1; i++)
+                {
+                    intervals.Add(temp[i + 1] - temp[i]);
+                }
+                MyModel.SetData(intervals);
+            }
         }
+
     }
 }
